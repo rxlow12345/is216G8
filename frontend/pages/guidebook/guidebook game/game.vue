fcd405f1d729d3e6ee669bfcb6f557eaf4bf0465
Charlize Teo <charlizeteo.2024@smu.edu.sg>
2025-10-26 17:10:08 +0800

    Fixed index.js issue

---
+++ frontend/pages/guidebook/guidebook game/game.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container-fluid p-0">
+    <div id = "logo">
+        <header class = "text-center">
+            <img src = "../images/logo.png">
+        </header>
+    </div>
+    <div id = "topBanner">
+    <header class="text-center mb-2">
+      <h1>🌿 Singapore Wildlife Guidebook</h1>
+      <p class="text-muted">Discover the amazing biodiversity of Singapore</p>
+    </header>
+    </div>
+    </div>
+</template>
