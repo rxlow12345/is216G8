ade5c10835ed7c6f567326352d390c0fd977f2ea
ameliasohh <amelia.soh.2024@smu.edu.sg>
2025-10-29 18:40:15 +0800

    Update index.js
    Added the route for Donate.vue

--- frontend/router/index.js
+++ frontend/router/index.js
@@ -5,6 +5,7 @@ import { onAuthStateChange } from '../src/api/auth.js';
 import App from '../src/App.vue'; // Default app
 import StatusUpdate from '../pages/orgAdmin/StatusUpdate.vue'; // StatusUpdate
 import Home from '../pages/Home.vue'
+import Donate from '../components/Donate.vue';
 import Login from '../pages/auth/Login.vue';
 import Signup from '../pages/auth/Signup.vue';
 import ReporterDashboard from '../pages/reporter/ReporterDashboard.vue';
@@ -75,7 +76,13 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home,
-  }
+  },
+    {
+    path: '/donate',
+    name: 'Donate',
+    component: Donate,
+    meta: { requiresAuth: false }
+  },
 ];
 
 // 2. Create the router instance
