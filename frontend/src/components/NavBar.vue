fbcf8348b8b3bf2d8cb7c3ef8e5ac973f561ef27
ryanc9029 <ryan.chua.2024@computing.smu.edu.sg>
2025-10-29 17:25:02 +0800

    Fix NavBar.vue
    Fixed logging out not clearing the auth state

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -61,6 +61,7 @@ export default {
     handleLogout() {
       try {
         logout();
+        this.auth = { email: null, role: null , uid: null};
         this.$router.push('/'); // redirect home
       } catch (err) {
         console.error(err);
