482458e2cfdaa909eb635562a95eb6331e647c8e
ryanc9029 <ryan.chua.2024@computing.smu.edu.sg>
2025-10-31 15:45:38 +0800

    Fix Report button in NavBar appearing for non reporters
    Fix Report button in NavBar appearing for non reporters

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -26,7 +26,6 @@
           <li class="nav-item"><router-link class="nav-link navLink" to="/" exact-active-class="active" @click="collapseNavbar">Home</router-link></li>
           <li class="nav-item"><router-link class="nav-link navLink" to="/guidebook" exact-active-class="active" @click="collapseNavbar">Guidebook</router-link></li>
           <li class="nav-item"><router-link class="nav-link navLink" to="/map" exact-active-class="active" @click="collapseNavbar">Map</router-link></li>
-          <li class="nav-item"><router-link class="nav-link navLink" to="/report" exact-active-class="active" @click="collapseNavbar">Report</router-link></li>
 
           <!-- Only if not logged in -->
           <li v-if="!auth.uid" class="nav-item"><router-link class="nav-link navLink" to="/login" exact-active-class="active" @click="collapseNavbar">Login</router-link></li>
