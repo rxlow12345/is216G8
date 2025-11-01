88fa97db9d6f21d0d8449882f90b00f75861ef4a
ryanc9029 <ryan.chua.2024@computing.smu.edu.sg>
2025-11-02 02:20:49 +0800

    Fixed NavBar responsiveness issues on smaller screens
    NavBar's fixed position covered part of page on smaller screens. Added dynamic height to prevent this.
    
    Changed NavBar to show hamburger icon on tablet as well

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,17 +6,19 @@
       <router-link to="/status-update">Go to Status Update</router-link>
       <a href="../pages/report.html">Report</a>
     </nav> -->
-    
+
     <header>
       <NavBar></NavBar>
     </header>
-    
+
     <!-- This is the magic placeholder! -->
     <!-- Your HomeView, MyNewView, etc., will be rendered right here -->
-     <main>
-      <router-view></router-view>
-     </main>
-    
+    <main>
+      <div :style="{ paddingTop: navbarHeight + 'px' }" class="page-content">
+        <router-view />
+      </div>
+    </main>
+
   </div>
 </template>
 
@@ -24,12 +26,32 @@
 import NavBar from './components/NavBar.vue';
 export default {
   name: 'App',
-  components: { NavBar }
+  components: { NavBar },
+  data() {
+    return {
+      navbarHeight: 0,
+    }
+  },
+  mounted() {
+    this.updateNavbarHeight();
+    window.addEventListener('resize', this.updateNavbarHeight);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.updateNavbarHeight);
+  },
+  methods: {
+    updateNavbarHeight() {
+      const navbar = document.querySelector('.customNavbar');
+      if (navbar) {
+        this.navbarHeight = navbar.offsetHeight;
+      }
+    }
+  }
 }
 </script>
 
 <style>
-main {
-  padding-top: 90px !important; /* Match navbar height */
+.page-content {
+  transition: padding-top 0.2s; /* smooth adjustment when resizing */
 }
 </style>
